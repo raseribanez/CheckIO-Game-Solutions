98cd108e76414bea5c186674fcc5dd428dde1b8b
Ben Woodfield <raser.13@hotmail.com>
2016-12-03 23:11:58 +0000

    Create indexPower.py

---
+++ indexPower.py
@@ -0,0 +1,132 @@
+# Mission 2
+# Get your own solutions
+
+# No..but seriously
+# If you want this code it is right here....somewhere, on this page 
+# You should solve thy riddle and you shall find what you seek
+
+# What goes up...Must come- ????  
+                                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Very Clever 
+
+|    |    |
+|    |    |
+|    |    |
+|    |    |
+V    V    V
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# So Here it really is - you are actually lucky that I would post this
+# not right that I had to do all the hard work...so I have completely messed it all up
+# Only messing, but you will have to do a little work if you want to use this code
+# Just read it and use your eyes to spot what's not right. 
+
+# Clue to solve the major malfunction in this code 
+# hoo = a
+# Just read the code and you will see what it means
+
+# Other than that this code will knock this mission into next week
+
+def index_power(hoorray, n):
+    if n == 0:
+        return 1
+    else:
+        if n >= len (hoorray):
+            return -1
+        else:
+            return hoorray[n]**n
+    return None
+
+    
