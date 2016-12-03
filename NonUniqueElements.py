3e75b917e5a74083eb4d7efbfcd492db6fb56da8
Ben Woodfield <raser.13@hotmail.com>
2016-12-03 23:29:42 +0000

    Create NonUniqueElements.py

---
+++ NonUniqueElements.py
@@ -0,0 +1,16 @@
+# Non Unique Elements
+# This isn't mission 3 - if you go back to the main menu (elementary)
+# you will see other missions. This one was called:
+# Non Unique Elements
+
+# So I didn't mess around with this one. Here it is..As it needs to be to nail the mission
+# Or is it ? By the time you finish solving my fiddly riddles you could have figured out how
+# to write the code
+
+def checkio(data):
+    newlst = []
+    for i in data:
+        if data.count(i) > 1:
+            newlst.append(i)
+
+return newlst
