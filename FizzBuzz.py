6f27af89684f6100ee2ac3e43c35e26c957b4810
Ben Woodfield <raser.13@hotmail.com>
2016-12-03 22:33:02 +0000

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,6 @@
+# Ben Woodfield
+# Get your own solutions by working it out
+# Seriously this one wasn't that hard - if you need help with the game interface email me at raserppsprograms@gmail.com
+# and I will talk you through the code and getting it to run in the game
+
+def checkio(number):
